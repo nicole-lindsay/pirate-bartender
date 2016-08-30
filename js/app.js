6201ce2785c1483651774aa126ff20acb92c25f9
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-08-30 09:03:31 -0400

    finally beginning

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+    var tastePref = {
+        'strong': 'Do ye like yer drinks strong?',
+        'salty': 'Do ye like it with a salty tang?',
+        'bitter': 'Are ye a lubber who likes it bitter?',
+        'sweet': 'Would ye like a bit of sweetness with yer poison?',
+        'fruity': 'Are ye one for a fruity finish?'
+    };
+
+    var drinkIngredients = {
+    	
+    }
 
 });
