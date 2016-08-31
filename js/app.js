f4e13e9fc8832d761fb13f2c6fe228798f4b70d5
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-08-31 09:15:55 -0400

    questions appear on the page now

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var tastePref = {
-    	'spicy': 'Are ye a fan of spicy drinks?',
+        'spicy': 'Are ye a fan of spicy drinks?',
         'strong': 'Do ye like yer drinks strong?',
         'salty': 'Do ye like it with a salty tang?',
         'bitter': 'Are ye a lubber who likes it bitter?',
@@ -18,27 +18,29 @@ $(document).ready(function() {
         this.ingredients = ingredients;
     };
 
-var ingredients = [
-	new Ingredient('spicy', 'ghost pepper'),
-	new Ingredient('strong', 'glug of rum'),
-	new Ingredient('strong', 'slug of whiskey'),
-	new Ingredient('strong', 'splash of gin'),
-	new Ingredient('salty', 'olive on a stick'),
-	new Ingredient('salty', 'salt-dusted rim'),
-	new Ingredient('salty', 'rasher of bacon'),
-	new Ingredient('bitter', 'shake of bitters'),
-	new Ingredient('bitter', 'splash of tonic'),
-	new Ingredient('bitter', 'twist of lemon peel'),
-	new Ingredient('sweet', 'sugar cube'),
-	new Ingredient('sweet', 'spoonful of honey'),
-	new Ingredient('sweet', 'splash of cola'),
-	new Ingredient('fruity', 'slice of orange'),
-	new Ingredient('fruity', 'dash of cassis'),
-	new Ingredient('fruity', 'cherry on top')
-];
+    var ingredients = [
+        new Ingredient('spicy', 'ghost pepper'),
+        new Ingredient('strong', 'glug of rum'),
+        new Ingredient('strong', 'slug of whiskey'),
+        new Ingredient('strong', 'splash of gin'),
+        new Ingredient('salty', 'olive on a stick'),
+        new Ingredient('salty', 'salt-dusted rim'),
+        new Ingredient('salty', 'rasher of bacon'),
+        new Ingredient('bitter', 'shake of bitters'),
+        new Ingredient('bitter', 'splash of tonic'),
+        new Ingredient('bitter', 'twist of lemon peel'),
+        new Ingredient('sweet', 'sugar cube'),
+        new Ingredient('sweet', 'spoonful of honey'),
+        new Ingredient('sweet', 'splash of cola'),
+        new Ingredient('fruity', 'slice of orange'),
+        new Ingredient('fruity', 'dash of cassis'),
+        new Ingredient('fruity', 'cherry on top')
+    ];
 
-var piratePantry = new Pantry(ingredients);
+    var piratePantry = new Pantry(ingredients);
 
-
+    for (var question in tastePref) {
+        $('#drinkQuestions').append(tastePref[question] + '<input type="checkbox" value="' + question + '"> <br>');
+    }
 
 });
